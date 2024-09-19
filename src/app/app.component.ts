254f3043ddc9dcc6c3f924843996f78be9261431
gpoongkuzhali <gpoongkuzhali@greatergoods.com>
2024-09-19 17:47:07 +0530

    Added sidebar nav menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../core/header/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MatSidenavModule, MatListModule, MatIconModule, MatCardModule, MatButtonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  shouldOpenDrawer = false;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  toggleDrawer() {
+    this.shouldOpenDrawer = !this.shouldOpenDrawer
+  }
 
 }
